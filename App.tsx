63c12a42272224a7f0e16953d75fbe8ce2233816
Abhishek Jangid <abhijan@Abhisheks-MacBook-Air.local>
2026-05-16 10:26:18 +0530

    Notification added

--- App.tsx
+++ App.tsx
@@ -3,13 +3,57 @@ import {
   SafeAreaView,
   StatusBar,
   StyleSheet,
+  PermissionsAndroid,
+  Platform
 } from 'react-native';
 import {LanguageProvider} from './src/localization/LanguageContext';
 import {AuthProvider} from './src/Backend/AuthContent';
 import {NavigationContainer} from '@react-navigation/native';
 import MainNavigation from './src/navigators/MainNavigation';
+import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
+ const requestUserPermission = async () => {
+  if (Platform.OS === 'android' && Platform.Version >= 33) {
+    await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+    );
+  }
+
+  const authStatus = await messaging().requestPermission();
+  console.log('Notification permission:', authStatus);
+};
+
+ const getFcmToken = async () => {
+  const token = await messaging().getToken();
+  console.log('FCM TOKEN:', token);
+  return token;
+};
+
+ const notificationListener = () => {
+  console.log("hiii");
+  
+  messaging().onMessage(async remoteMessage => {
+    console.log('Foreground notification:', remoteMessage);
+  });
+
+  messaging().onNotificationOpenedApp(remoteMessage => {
+    console.log('Opened from background:', remoteMessage);
+  });
+
+  messaging()
+    .getInitialNotification()
+    .then(remoteMessage => {
+      if (remoteMessage) {
+        console.log('Opened from quit state:', remoteMessage);
+      }
+    });
+};
+  useEffect(() => {
+    requestUserPermission();
+    getFcmToken();
+    notificationListener();
+  }, []);
 
   return (
     <LanguageProvider>
